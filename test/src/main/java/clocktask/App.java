b119936f5f571228baa2591a2427dc5fcb32e20c
Ra'Uko <raukotaur@gmail.com>
2016-04-05 21:05:30 +0300

    Implemented logging, not implemented unit-tests

--- test/src/main/java/clocktask/App.java
+++ test/src/main/java/clocktask/App.java
@@ -6,18 +6,27 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import org.apache.log4j.Logger;
+
 public class App {
+	
+	private static final Logger log = Logger.getLogger(App.class);
+	
 	public static void main(String[] args){
 		Locale defaultLocale = Locale.getDefault();
 		ResourceBundle bundle = null;
-		if (Locale.getDefault().getDisplayLanguage().equals("русский")) {
+		if (Locale.getDefault().getDisplayLanguage().equals("русский") || Locale.getDefault().getDisplayLanguage().equals("українська")) {
 			bundle = ResourceBundle.getBundle("TestBundle_ru_RU", defaultLocale);
+			log.info("Default locale is " + defaultLocale);
 			displayValues(bundle);
-		} else if (Locale.getDefault().getDisplayLanguage().equals("українська")) {
+	/*	} else if (Locale.getDefault().getDisplayLanguage().equals("українська")) {
 			bundle = ResourceBundle.getBundle("TestBundle_ru_UA", defaultLocale);
-			displayValues(bundle);
-		} else if (Locale.getDefault().getDisplayLanguage().equals("English")) {
+			log.info("Default locale is " + defaultLocale);
+			displayValues(bundle);	*/
+		} else /* if (Locale.getDefault().getDisplayLanguage().equals("English"))*/ {
 			bundle = ResourceBundle.getBundle("TestBundle_en_US", defaultLocale);
+			log.info("Default locale is " + defaultLocale);
+			log.error("This locale is not supported. Communication with user will be in English");
 			displayValues(bundle);
 		}
     }
@@ -28,10 +37,16 @@ public class App {
 		int currentHour = Integer.parseInt(dateFormat.format(date));
 		if(currentHour >= 6 && currentHour < 9){
 			System.out.println(bundle.getString("my.morning"));
+			log.info("Program replied : " + bundle.getString("my.morning"));
 		} else if (currentHour >= 9 && currentHour < 19){
 			System.out.println(bundle.getString("my.day"));
+			log.info("Program replied : " + bundle.getString("my.day"));
 		} else if (currentHour >= 19 && currentHour < 23){
 			System.out.println(bundle.getString("my.evening"));
-		} else System.out.println(bundle.getString("my.night"));
+			log.info("Program replied : " + bundle.getString("my.evening"));
+		} else {
+			System.out.println(bundle.getString("my.night"));
+			log.info("Program replied : " + bundle.getString("my.night"));
+		}
 	}
 }
